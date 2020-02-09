9774c62fd0db68c6805b7b2c9d44ebd20c827b51
andreacampaci <!Dimensione89>
2020-02-09 18:27:10 +0100

    add react preset and libs

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 require('babel-runtime/regenerator');
+require('babel-register');
 require('webpack-hot-middleware/client?reload=true');
 require('./main.css');
 require('./index.html');
+require('./app.js');
 
 
 const a = async (args) => {
