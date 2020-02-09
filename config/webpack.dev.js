db7d16500cf83a55a3dee172d3b63012d4f03c35
andreacampaci <!Dimensione89>
2020-02-09 15:19:19 +0100

    webpack dev server basic

---
+++ config/webpack.dev.js
@@ -0,0 +1,62 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        main: "./src/main.js"
+    },
+    mode: "development",
+    output: {
+        filename: "[name]-bundle.js",
+        path: path.resolve(__dirname, "../dist"),
+        publicPath: ""
+    },
+    devServer: {
+        contentBase: "dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].html"
+                        }
+                    },
+                    {
+                        loader: "extract-loader"
+                    },
+                    {
+                        loader: "html-loader",
+                        options: {
+                            attrs: ["img:src"]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg|gif|png)$/,
+                use: [
+                    {
+                        loader: "file-loader" ,
+                        options: {
+                            name: "[name]-[hash:8].[ext]",
+                        }
+                    },
+                ]
+            }
+        ]
+    }
+}
